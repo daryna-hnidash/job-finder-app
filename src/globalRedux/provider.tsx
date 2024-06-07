1c8fa70be7410cd77f65c5e835b08ad6aca3d27c
Daryna <108665898+daryna-hnidash@users.noreply.github.com>
2024-06-07 14:38:27 +0300

    Update provider.tsx
    {children} type added

--- src/globalRedux/provider.tsx
+++ src/globalRedux/provider.tsx
@@ -3,38 +3,14 @@
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-export function Providers({ children }) {
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
     return (
         <Provider store={store}>
             {children}
         </Provider>
     )
 }
-
-// 'use client'
-
-// import { useRef } from 'react'
-// import { Provider } from 'react-redux'
-
-// import { PersistGate } from 'redux-persist/integration/react'
-// import { AppStore, makeStore } from './store'
-
-// export default function StoreProvider({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   const storeRef = useRef<AppStore>()
-//   if (!storeRef.current) {
-//     // Create the store instance the first time this renders
-//     storeRef.current = makeStore()
-//   }
-
-//   return (
-//     <Provider store={storeRef.current}>
-//       <PersistGate loading={null} persistor={storeRef.current.__persistor}>
-//         {children}
-//       </PersistGate>
-//     </Provider>
-//   )
-// }
